8f6cb2919ddf4d3cc2a904dbc0c4bd19fc1f478f
Toreole <toreole.official@gmail.com>
2022-07-29 17:18:22 +0200

    Knight Movement

--- ConsoleChess/ConsoleChess/King.cpp
+++ ConsoleChess/ConsoleChess/King.cpp
@@ -1,6 +1,22 @@
 #include "King.h"
+#include <cmath>
 
 bool ConsoleChess::King::CanMoveTo(int tx, int ty, std::array<std::array<ChessPiece*, 8>, 8>* board)
 {
+    int deltaX = tx - row;
+    int deltaY = ty - column;
+
+    if (std::abs(deltaX) <= 1 && std::abs(deltaY) <= 1)
+    {
+        ChessPiece* other = board->at(tx).at(ty);
+        if (other != nullptr)
+        {
+            if (other->color == color)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
     return false;
 }
--- ConsoleChess/ConsoleChess/Knight.cpp
+++ ConsoleChess/ConsoleChess/Knight.cpp
@@ -1,6 +1,22 @@
 #include "Knight.h"
+#include <cmath>
 
 bool ConsoleChess::Knight::CanMoveTo(int tx, int ty, std::array<std::array<ChessPiece*, 8>, 8>* board)
 {
+    int dx = tx - row;
+    int dy = ty - column;
+    int adx = std::abs(dx);
+    int ady = std::abs(dy);
+
+    if (adx == 2 && ady == 1 || adx == 1 && ady == 2)
+    {
+        ChessPiece* other = board->at(tx).at(ty);
+        if (other != nullptr)
+        {
+            if (other->color == color)
+                return false;
+        }
+        return true;
+    }
     return false;
 }
