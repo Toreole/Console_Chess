349446d537c675c03e5110285c3994a4ef84fe2e
Toreole <toreole.official@gmail.com>
2022-07-30 17:24:04 +0200

    Rook movement

--- ConsoleChess/ConsoleChess/Rook.cpp
+++ ConsoleChess/ConsoleChess/Rook.cpp
@@ -1,9 +1,47 @@
 #include "Rook.h"
 #include <cmath>
+#include <iostream>
 
 using namespace ConsoleChess;
 
 bool ConsoleChess::Rook::CanMoveTo(int tx, int ty, std::array<std::array<ChessPiece*, 8>, 8>* board)
 {
-	return false;
+    int dx = tx - row;
+    int dy = ty - column;
+
+    int adx = std::abs(dx);
+    int ady = std::abs(dy);
+
+    if ((dx == 0 && dy != 0) || (dy == 0 && dx != 0))
+    {
+        int xDir = dx > 0 ? 1 : dx < 0 ? -1 : 0;
+        int yDir = dy > 0 ? 1 : dy < 0 ? -1 : 0;
+
+        int xx = row + xDir;
+        int yy = column + yDir;
+
+        //all positions between the start and the destination.
+        for (; xx != tx || yy != ty; xx += xDir, yy += yDir)
+        {
+            ChessPiece* other = board->at(xx).at(yy);
+            if (other != nullptr)
+            {
+                std::cout << "piece in the way." << std::endl;
+                return false;
+            }
+        }
+        //the final position.
+        ChessPiece* other = board->at(tx).at(yy);
+        if (other != nullptr)
+        {
+            std::cout << "cant take ally" << std::endl;
+            return other->color != color;
+        }
+        return true;
+    }
+    else
+    {
+        std::cout << "not a linear move: " << adx << " | " << ady << std::endl;
+    }
+    return false;
 }
