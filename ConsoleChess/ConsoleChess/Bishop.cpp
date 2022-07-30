bfb37998f10abc78cc34cb2c677a26f9565df894
Toreole <toreole.official@gmail.com>
2022-07-30 16:11:57 +0200

    Bishop movement

--- ConsoleChess/ConsoleChess/Bishop.cpp
+++ ConsoleChess/ConsoleChess/Bishop.cpp
@@ -1,6 +1,46 @@
 #include "Bishop.h"
+#include <cmath>
+#include <iostream>
 
 bool ConsoleChess::Bishop::CanMoveTo(int tx, int ty, std::array<std::array<ChessPiece*, 8>, 8>* board)
 {
+
+    int dx = tx - row;
+    int dy = ty - column;
+
+    int adx = std::abs(dx);
+    int ady = std::abs(dy);
+
+    if (adx == ady && adx != 0)
+    {
+        int xDir = dx > 0 ? 1 : -1;
+        int yDir = dy > 0 ? 1 : -1;
+
+        int xx = row + xDir;
+        int yy = column + yDir;
+
+        //all positions between the start and the destination.
+        for (; xx != tx; xx += xDir, yy += yDir)
+        {
+            ChessPiece* other = board->at(xx).at(yy);
+            if (other != nullptr)
+            {
+                std::cout << "piece in the way." << std::endl;
+                return false;
+            }
+        }
+        //the final position.
+        ChessPiece* other = board->at(tx).at(yy);
+        if (other != nullptr)
+        {
+            std::cout << "cant take ally" << std::endl;
+            return other->color != color;
+        }
+        return true;
+    }
+    else
+    {
+        std::cout << "not a diagonal move: " << adx << " | " << ady << std::endl;
+    }
     return false;
 }
--- ConsoleChess/ConsoleChess/Rook.cpp
+++ ConsoleChess/ConsoleChess/Rook.cpp
@@ -1,8 +1,9 @@
 #include "Rook.h"
+#include <cmath>
 
 using namespace ConsoleChess;
 
 bool ConsoleChess::Rook::CanMoveTo(int tx, int ty, std::array<std::array<ChessPiece*, 8>, 8>* board)
 {
-    return false;
+	return false;
 }
